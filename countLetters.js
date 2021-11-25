2fb641407bf4af383bd0c5e932dd09706fe74db1
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-11-25 01:25:38 +0000

    complete design of countLetters

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// sentence is string
+// purpose: return count of each of the letters in given sentence as an object
+const countLetters = function(sentence) {
+  let results = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+assertEqual(countLetters("lighthouse")["h"], 2);
+assertEqual(countLetters("lighthouse")["i"], 1);
+assertEqual(countLetters("lighthouse")["x"], undefined);
+assertEqual(countLetters("functions objects conditionals arrays loops")["a"], 3);
