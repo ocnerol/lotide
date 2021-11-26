18d0f7136ea35780dc72a55fcb6e013bdbc102bd
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-11-26 03:18:06 +0000

    move eqArrays fn into assertArraysEqual fn to move them together as one fn

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,17 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+const assertArraysEqual = function(arr1, arr2) {
+
+  const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length) {
       return false;
     }
-  }
-  return true;
-};
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
 
-const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log("✅✅✅ The given arrays ", arr1, " and ", arr2, " are equal!");
   } else {
