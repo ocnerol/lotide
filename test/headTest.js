6a73e08d17910d2e37e4af15d9c0d037b0069c21
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-11-30 01:13:36 +0000

    imported Mocha and Chai and converted headTest.js to use Mocha and Chai for unit tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-// TEST CODE
-assertEqual(head([]), undefined);
-assertEqual(head([]), null);
-assertEqual(head([3]), 3);
-assertEqual(head(["this", "old", "dog"]), "old");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
+const head = require('../head')
+
+describe('#head', () => {
+
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it('should return undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it('should return "abc" for ["abc", "def", "ghi"]', () => {
+    assert.strictEqual(head(["abc", "def", "ghi"]), "abc");
+  });
+});
